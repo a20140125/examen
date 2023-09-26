56d701f3ad32d1f7ee6d6b515d6a1f21b1e17bf1
a20140125 <131193606+a20140125@users.noreply.github.com>
2023-09-26 12:43:11 -0500

    Add files via upload

---
+++ evalua.js
@@ -0,0 +1,6 @@
+var correctas = parseInt(prompt(" ¿En qué departamento se encuentra el lago Titicaca? "));
+var cusco = parseInt(prompt("correcto"));
+var tacna = parseInt(prompt("incorrecto"));
+
+
+
